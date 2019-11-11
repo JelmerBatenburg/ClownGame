687ab548228953ed2952c011ac6d713879171a24
JelmerBatenburg <jelmer.batenburg@gmail.com>
2019-11-11 15:31:06 +0100

    Main Menu base

---
+++ ClownGame/Assets/Scripts/MainMenu/BasicUIFunctions.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BasicUIFunctions : MonoBehaviour
+{
+    [Header("FromToButton")]
+    public GameObject[] from;
+    public GameObject[] to;
+    [Header("SizeChange")]
+    public float sizeIncrease;
+    private Vector3 basicSize;
+    [Header("ChangeColor")]
+    public Color highlightedColor;
+    public Color nonHighlightedColor;
+
+    public void FromToButton()
+    {
+        foreach (GameObject obj in from)
+            obj.SetActive(false);
+        foreach (GameObject obj in to)
+            obj.SetActive(true);
+    }
+
+    public void SizeChangeEnter(GameObject obj)
+    {
+        basicSize = obj.transform.localScale;
+        obj.transform.localScale = basicSize * sizeIncrease;
+    }
+
+    public void SizeChangeExit(GameObject obj)
+    {
+        obj.transform.localScale = basicSize;
+    }
+
+    public void HighlightColor(GameObject obj)
+    {
+        obj.GetComponent<Image>().color = highlightedColor;
+    }
+
+    public void DeHighlightColor(GameObject obj)
+    {
+        obj.GetComponent<Image>().color = nonHighlightedColor;
+    }
+}
