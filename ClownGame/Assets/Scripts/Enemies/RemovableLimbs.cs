22b6d4b85c36bdfa5c214f978b94e20083b593f2
JelmerBatenburg <jelmer.batenburg@gmail.com>
2019-11-14 21:00:49 +0100

    teleurstellend

---
+++ ClownGame/Assets/Scripts/Enemies/RemovableLimbs.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemovableLimbs : MonoBehaviour
+{
+    public RemovableLimbInformation[] removableLimbs;
+    public float health;
+
+    public void DoDamage(Collider col, float damage)
+    {
+        bool normalDamage = true;
+        foreach(RemovableLimbInformation info in removableLimbs)
+            if(col == info.col)
+            {
+                normalDamage = false;
+                health -= damage *= info.damageMultiplier;
+                if (health <= 0)
+                    info.obj.SetActive(false);
+                break;
+            }
+
+        if (normalDamage)
+            health -= damage;
+    }
+
+    public class RemovableLimbInformation
+    {
+        public Collider col;
+        public GameObject obj;
+        public float damageMultiplier = 1;
+    }
+}
---
+++ ClownGame/Assets/Scripts/Player/CharacterMovement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    [Header("Movement")]
+    public float movementSpeed;
+    Vector3 movementDirection;
+    [Header("GroundDetection")]
+    public float jumpHeight;
+    public string jumpInput;
+    public float downwardsRaycastRange;
+    public LayerMask groundMask;
+    public Rigidbody rig;
+    [Header("Rotation")]
+    public Transform cam;
+    public float rotationSpeed;
+    [Header("WeaponSway")]
+    public float weaponSwayStrenght;
+    public Transform weapon;
+    public float weaponLerpSpeed;
+    public float weaponJumpWeight;
+
+    public void Update()
+    {
+        Move();
+        Rotate();
+        WeaponSway();
+    }
+
+    public void WeaponSway()
+    {
+        weapon.Translate(new Vector3(-Input.GetAxis("Mouse X"), -Input.GetAxis("Mouse Y") - (rig.velocity.y * weaponJumpWeight)) * weaponSwayStrenght * Time.deltaTime);
+        weapon.position = Vector3.Lerp(weapon.position, weapon.parent.position, Time.deltaTime * weaponLerpSpeed);
+    }
+
+    public void Rotate()
+    {
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime * Input.GetAxis("Mouse X"));
+        cam.Rotate(Vector3.right * rotationSpeed * Time.deltaTime * -Input.GetAxis("Mouse Y"));
+    }
+
+    public void Move()
+    {
+        if (Physics.Raycast(transform.position, Vector3.down, downwardsRaycastRange, groundMask))
+        {
+            movementDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            if (Input.GetButtonDown(jumpInput))
+                rig.velocity += Vector3.up * jumpHeight;
+        }
+        transform.Translate(movementDirection * Time.deltaTime * movementSpeed);
+    }
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.DrawRay(transform.position, Vector3.down);
+    }
+}
