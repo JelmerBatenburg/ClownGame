3c07982c21974486335bf80908760f1e9dd8c91e
JelmerBatenburg <jelmer.batenburg@gmail.com>
2019-11-18 11:46:36 +0100

    TestTime

--- ClownGame/Assets/Scripts/Player/CharacterMovement.cs
+++ ClownGame/Assets/Scripts/Player/CharacterMovement.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterMovement : MonoBehaviour
+public class CharacterMovement : Photon.MonoBehaviour
 {
     [Header("Movement")]
     public float movementSpeed;
@@ -16,17 +16,46 @@ public class CharacterMovement : MonoBehaviour
     [Header("Rotation")]
     public Transform cam;
     public float rotationSpeed;
+    [Header("Networking")]
+    public GameObject[] enableObjects;
+    public GameObject[] disabledObjects;
+    public float lerpSpeed;
+    Vector3 position;
+    Quaternion rotation;
+    Quaternion camRotation;
 
     public void Start()
     {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+        if (!photonView.isMine)
+        {
+            foreach (GameObject obj in enableObjects)
+                obj.SetActive(true);
+            foreach (GameObject obj in disabledObjects)
+                obj.SetActive(false);
+            StartCoroutine(LerpPosition());
+        }
+    }
+
+    public IEnumerator LerpPosition()
+    {
+        while (true)
+        {
+            transform.position = Vector3.Lerp(transform.position, position, Time.deltaTime * lerpSpeed);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * lerpSpeed);
+            cam.rotation = Quaternion.Lerp(cam.rotation, camRotation, Time.deltaTime * lerpSpeed);
+            yield return null;
+        }
     }
 
     public void Update()
     {
-        Move();
-        Rotate();
+        if (photonView.isMine)
+        {
+            Move();
+            Rotate();
+        }
     }
 
     public void Rotate()
@@ -44,6 +73,23 @@ public class CharacterMovement : MonoBehaviour
                 rig.velocity += Vector3.up * jumpHeight;
         }
         transform.Translate(movementDirection * Time.deltaTime * movementSpeed);
+    }
+
+    void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+            stream.SendNext(cam.rotation);
+        }
+        else
+        {
+            position = (Vector3)stream.ReceiveNext();
+            rotation = (Quaternion)stream.ReceiveNext();
+            cam.rotation = (Quaternion)stream.ReceiveNext();
+        }
+
     }
 
     public void OnDrawGizmos()
