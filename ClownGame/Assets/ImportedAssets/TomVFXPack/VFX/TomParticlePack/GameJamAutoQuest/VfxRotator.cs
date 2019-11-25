5ebeb74069b20f0344b080e715e2bb8acb0b817e
ThomasBliekendaal <33858982+ThomasBliekendaal@users.noreply.github.com>
2019-11-25 11:45:11 +0100

    particles jaaaaaaaaaaa

---
+++ ClownGame/Assets/ImportedAssets/TomVFXPack/VFX/TomParticlePack/GameJamAutoQuest/VfxRotator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VfxRotator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.rotation = Quaternion.Euler(-90, 0, 0);
+    }
+
+}
