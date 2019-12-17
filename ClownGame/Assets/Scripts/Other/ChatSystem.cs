30571befd69c3abc1feacc4de4de44fef9dc63b0
JelmerBatenburg <jelmer.batenburg@gmail.com>
2019-12-17 15:29:27 +0100

    ChatPush

---
+++ ClownGame/Assets/Scripts/Other/ChatSystem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChatSystem : Photon.MonoBehaviour
+{
+    public InputField inputField;
+    public GameObject chatMessage;
+    public Transform chatLayout;
+    public float chatMessageLifetime;
+
+    public void SendMessage()
+    {
+        photonView.RPC("MessageRecieve", PhotonTargets.All, inputField.text);
+        inputField.text = "";
+    }
+
+    [PunRPC,HideInInspector]
+    public void MessageRecieve(string message)
+    {
+        GameObject g = Instantiate(chatMessage, chatLayout);
+        g.GetComponent<Text>().text = message;
+        Destroy(g, chatMessageLifetime);
+    }
+}
